133147e4eaa4cd4509e9fc95cb43615de9347ab2
DEEPANSHU MAURYA <149106139+Mr-Peter0084@users.noreply.github.com>
2025-09-01 17:59:28 +0000

    day one learning

--- basic/test.js
+++ basic/test.js
@@ -1,1 +1,6 @@
+"use strict"; // optional h , and  tell that now there will be use of standard or new version of js from now  
+
+alert(3+4) // qki ye code ye node-js  me chlega , isiliye error aayega but console me chl jayega qki vo browser me run hoti h 
+
+
 console.log("Deepanshu MAurya")
